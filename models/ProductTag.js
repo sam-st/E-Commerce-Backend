89a38dc4eb1f636defcd9af7b4c53b8ed4893dc7
sam <sammsfs@gmail.com>
2023-08-28 17:26:44 -0400

    added models for the database

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,12 +2,32 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: 'id',
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
